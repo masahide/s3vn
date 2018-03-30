c52dbf84e49455ab216aa0ac70ee92424625ece3
YAMASAKI Masahide <masahide.y@gmail.com>
2018-03-30 10:31:08 +0900

    add

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"crypto/sha1"
+	"io"
 	"os"
 	"path/filepath"
 	"syscall"
 	"time"
 
-	"github.com/k0kubun/pp"
+	"github.com/pkg/errors"
+)
+
+const (
+	HashSize = sha1.Size
 )
 
 // FsJEvxFGrgEnbNKNodx9mAjgudA=/path/to/filename
@@ -16,7 +21,7 @@ import (
 type FileInfo struct {
 	Mode   os.FileMode
 	Path   string
-	Xxhash []byte
+	Hash   [HashSize]byte
 	Size   int64
 	Mtime  time.Time
 	LinkTo string
@@ -30,11 +35,26 @@ type fileInfos struct {
 	Files []FileInfo
 }
 
-func (f *fileInfos) append(file FileInfo) {
-	f.Files = append(f.Files, file)
+func makeHash(path string) ([]byte, error) {
+	res := []byte{}
+	f, err := os.Open(path)
+	if err != nil {
+		return res, err
+	}
+	defer f.Close()
+
+	h := sha1.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return res, err
+	}
+	return h.Sum(nil), nil
 }
 
-func (f *fileInfos) walk(path string, info os.FileInfo, err error) error {
+func (fs *fileInfos) append(file FileInfo) {
+	fs.Files = append(fs.Files, file)
+}
+
+func (fs *fileInfos) walk(path string, info os.FileInfo, err error) error {
 	if info.IsDir() {
 		// 特定のディレクトリ以下を無視する場合は
 		// return filepath.SkipDir
@@ -54,17 +74,21 @@ func (f *fileInfos) walk(path string, info os.FileInfo, err error) error {
 			return err
 		}
 	}
-	f.append(fi)
+	fs.append(fi)
 	return nil
 }
 
 func main() {
+	makeFileInfos(os.Args[1])
+}
+
+func makeFileInfos(dir string) ([]FileInfo, error) {
 	fs := &fileInfos{
 		Files: make([]FileInfo, 0, 10000),
 	}
 	err := filepath.Walk(dir, fs.walk)
 	if err != nil {
-		fmt.Println(1, err)
+		return nil, errors.Wrap(err, "Failed makeFileInfos")
 	}
-	pp.Print(fs)
+	return fs.Files, nil
 }
